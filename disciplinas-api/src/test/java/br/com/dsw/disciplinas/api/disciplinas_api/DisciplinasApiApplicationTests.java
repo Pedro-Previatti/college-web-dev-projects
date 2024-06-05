90513a2db4359d9d6094e190a193394bbe162d06
Pedro Previatti <pvlpreviatti@gmail.com>
2024-06-05 14:22:19 -0300

    initial commit

---
+++ disciplinas-api/src/test/java/br/com/dsw/disciplinas/api/disciplinas_api/DisciplinasApiApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.dsw.disciplinas.api.disciplinas_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DisciplinasApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

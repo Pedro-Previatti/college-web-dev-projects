30514f7e0e0a8455945489e47ef469670016cce7
Pedro Previatti <pvlpreviatti@gmail.com>
2024-06-05 16:51:36 -0300

    feat(api/resource): added parameter validation for POST and added DELETE endpoint

--- disciplinas-api/src/main/java/br/com/dsw/disciplinas/api/resource/DisciplinaResource.java
+++ disciplinas-api/src/main/java/br/com/dsw/disciplinas/api/resource/DisciplinaResource.java
@@ -7,6 +7,7 @@ import java.util.Optional;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
@@ -47,4 +48,9 @@ public class DisciplinaResource {
     return ResponseEntity.created(uri).body(disciplinaGravada);
   }
 
+  @DeleteMapping("/{id}")
+  @ResponseStatus(HttpStatus.NO_CONTENT)
+  public void remover(@PathVariable Long id) {
+    disciplinaRepository.deleteById(id);
+  }
 }

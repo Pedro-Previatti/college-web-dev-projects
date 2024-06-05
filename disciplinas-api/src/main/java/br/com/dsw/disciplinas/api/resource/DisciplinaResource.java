bbc354a3db0b2de68d4986dd2c3e20f2093c9ed3
Pedro Previatti <pvlpreviatti@gmail.com>
2024-06-05 16:17:34 -0300

    feat(api/resource): added proper mapping for rest controller

--- disciplinas-api/src/main/java/br/com/dsw/disciplinas/api/resource/DisciplinaResource.java
+++ disciplinas-api/src/main/java/br/com/dsw/disciplinas/api/resource/DisciplinaResource.java
@@ -4,10 +4,14 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 import br.com.dsw.disciplinas.api.model.Disciplina;
 import br.com.dsw.disciplinas.api.repository.DisciplinaRepository;
+import org.springframework.web.bind.annotation.RequestMapping;
 
+@RestController
+@RequestMapping("/disciplinas")
 public class DisciplinaResource {
   @Autowired
   private DisciplinaRepository disciplinaRepository;

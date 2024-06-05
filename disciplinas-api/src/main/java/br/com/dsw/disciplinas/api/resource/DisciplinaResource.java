e096764a3a3eb24a1c995c0d1a4f28633d6e6de8
Pedro Previatti <pvlpreviatti@gmail.com>
2024-06-05 17:03:24 -0300

    feat(api/resource): added PUT endpoint

--- disciplinas-api/src/main/java/br/com/dsw/disciplinas/api/resource/DisciplinaResource.java
+++ disciplinas-api/src/main/java/br/com/dsw/disciplinas/api/resource/DisciplinaResource.java
@@ -15,18 +15,23 @@ import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import br.com.dsw.disciplinas.api.model.Disciplina;
 import br.com.dsw.disciplinas.api.repository.DisciplinaRepository;
+import br.com.dsw.disciplinas.api.service.DisciplinaService;
 import jakarta.servlet.http.HttpServletResponse;
+import jakarta.validation.Valid;
 
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.PutMapping;
 
 @RestController
 @RequestMapping("/disciplinas")
 public class DisciplinaResource {
   @Autowired
   private DisciplinaRepository disciplinaRepository;
+  @Autowired
+  private DisciplinaService disciplinaService;
 
   @GetMapping
   public List<Disciplina> listar() {
@@ -52,5 +57,11 @@ public class DisciplinaResource {
   @ResponseStatus(HttpStatus.NO_CONTENT)
   public void remover(@PathVariable Long id) {
     disciplinaRepository.deleteById(id);
+  }
+
+  @PutMapping("/{id}")
+  public ResponseEntity<?> atualizar(@PathVariable Long id, @Valid @RequestBody Disciplina disciplina) {
+    Disciplina disciplinaGravada = disciplinaService.atualizar(id, disciplina);
+    return ResponseEntity.ok(disciplinaGravada);
   }
 }

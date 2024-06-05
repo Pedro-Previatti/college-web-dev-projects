703014b538ace22db962d6262f0c47b56b6713a4
Pedro Previatti <pvlpreviatti@gmail.com>
2024-06-05 15:45:41 -0300

    feat(resources/db/migrations): migration v1 with flywaydb

---
+++ disciplinas-api/src/main/resources/db/migration/V01__criar_e_registrar_disciplinas.sql
@@ -0,0 +1,10 @@
+CREATE TABLE disciplina(
+  id SERIAL PRIMARY KEY,
+  nome VARCHAR(50) NOT NULL
+);
+
+INSERT INTO disciplina (nome) VALUES ('Algoritimos e Programacao Estruturada');
+INSERT INTO disciplina (nome) VALUES ('Desenvolvimento de Solucoes Web');
+INSERT INTO disciplina (nome) VALUES ('Analise e Projeto de Sistemas');
+INSERT INTO disciplina (nome) VALUES ('Arquitetura de Software');
+INSERT INTO disciplina (nome) VALUES ('Banco de Dados');
